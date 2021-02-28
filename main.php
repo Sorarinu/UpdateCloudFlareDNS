075cdb9c8ea91111c8d6fdc5b4e1cfc2fea74bbc
Sorarinu <sorarinu0814@gmail.com>
2021-02-28 17:35:36 +0900

    fix: ブログドメイン削除

--- main.php
+++ main.php
@@ -8,7 +8,6 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $records = [
-    'sorarinu.dev'   => true,
     'tv.sorarinu.dev'   => true,
     'home.sorarinu.dev' => false,
     'krishna-api.sorarinu.dev' => true,
